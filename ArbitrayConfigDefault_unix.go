7a6e80efb84a7f9aa6eb07588b472a4979e3031b
kts of kettek (Tyger) <kettek1@kettek.net>
2019-01-29 17:05:08 -0800

    Fix unix defaults; ignore some fields in json

--- ArbitrayConfigDefault_unix.go
+++ ArbitrayConfigDefault_unix.go
@@ -3,7 +3,10 @@
 package main
 
 func (c *ArbitrayConfig) generateDefault() (err error) {
-  c.Programs = make([]ArbitrayProgram, 1)
-  c.Programs[0].Arguments = make([]string, 0)
+  c.Programs = make([]*ArbitrayProgram, 1)
+  c.Programs[0] = &ArbitrayProgram{
+    Title: "XTerm",
+    Program: "xterm",
+  }
   return
 }
--- ArbitrayProgram.go
+++ ArbitrayProgram.go
@@ -11,8 +11,8 @@ type ArbitrayProgram struct {
   Program string `json:"program,omitempty"`
   Arguments []string `json:"arguments,omitempty"`
   Options ArbitrayProgramOptions `json:"options,omitempty"`
-  MenuItem *systray.MenuItem
-  Cmd *exec.Cmd
-  CloseChan chan bool
-  KillChan chan bool
+  MenuItem *systray.MenuItem `json:"-"`
+  Cmd *exec.Cmd `json:"-"`
+  CloseChan chan bool `json:"-"`
+  KillChan chan bool `json:"-"`
 }
